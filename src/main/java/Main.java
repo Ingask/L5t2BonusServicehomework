274591366f03d7cff6c2f6833225610eb475ba55
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2020-10-01 16:05:28 +0300

    fixed #1 Code edited

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
 
-    service.calculate(amount, registered);
-  }
+        //Добавленный код
+
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        boolean passed = expected == actual;
+        System.out.println(passed);
+
+
+    }
 }
 
